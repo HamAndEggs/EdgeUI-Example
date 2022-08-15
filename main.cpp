4ec7ae8f7ad5f5a203a3974fb5f7fe91c8721251
Richard Collins <richard@hex-edge.com>
2022-08-15 15:59:18 +0100

    Added new controls

--- main.cpp
+++ main.cpp
@@ -18,6 +18,8 @@
 #include "Graphics.h"
 #include "Element.h"
 #include "controls/Button.h"
+#include "controls/CheckBox.h"
+#include "controls/RadioButton.h"
 #include "Application.h"
 
 #include <unistd.h>
@@ -39,10 +41,19 @@ private:
     const float RECT_RADIUS = 0.2f;
     const float BORDER_SIZE = 3.0f;
     const std::string mPath;
+    bool mAnimate = false;
     eui::ElementPtr mRoot = nullptr;
 
     eui::ElementPtr MakeBouncyIcon(eui::Graphics* pGraphics);
+    eui::ElementPtr MakeOptions(int pFont);
+    eui::ElementPtr MakeRadio(int pFont);
 
+    uint32_t banana = 0;
+    uint32_t cherry = 0;
+    uint32_t grapes = 0;
+    uint32_t orange = 0;
+
+    uint32_t mCurrentImage = 0;
 };
 
 MyUI::MyUI(const std::string& path):mPath(path)
@@ -57,11 +68,18 @@ MyUI::~MyUI()
 
 void MyUI::OnOpen(eui::Graphics* pGraphics)
 {
+
     mRoot = new eui::Element;
     mRoot->SetID("mainScreen");
     mRoot->SetGrid(3,3);
 
-//    int miniFont = pGraphics->FontLoad(mPath + "liberation_serif_font/LiberationSerif-Regular.ttf",25);
+    banana = pGraphics->TextureLoadPNG(mPath + "./icons/banana.png");
+    cherry = pGraphics->TextureLoadPNG(mPath + "./icons/cherry.png");
+    grapes = pGraphics->TextureLoadPNG(mPath + "./icons/grapes.png");
+    orange = pGraphics->TextureLoadPNG(mPath + "./icons/orange.png");
+    mCurrentImage = orange;
+
+    int miniFont = pGraphics->FontLoad(mPath + "liberation_serif_font/LiberationSerif-Regular.ttf",25);
     int normalFont = pGraphics->FontLoad(mPath + "liberation_serif_font/LiberationSerif-Regular.ttf",40);
     int largeFont = pGraphics->FontLoad(mPath + "liberation_serif_font/LiberationSerif-Bold.ttf",100);
 
@@ -79,6 +97,14 @@ void MyUI::OnOpen(eui::Graphics* pGraphics)
     })->SetPadding(0.05f));
 
     mRoot->Attach(MakeBouncyIcon(pGraphics)->SetPos(1,0));
+    mRoot->Attach(MakeOptions(miniFont)->SetPos(2,0));
+    mRoot->Attach(MakeRadio(miniFont)->SetPos(0,1));
+
+    mRoot->Attach((new eui::Element)->SetOnUpdate([this](eui::ElementPtr e)
+    {
+        e->GetStyle().mTexture = mCurrentImage;
+        return true;
+    })->SetPos(1,1)->SetPadding(0.04f));
 
 }
 
@@ -93,18 +119,58 @@ eui::ElementPtr MyUI::MakeBouncyIcon(eui::Graphics* pGraphics)
     eui::Style s;
     s.mTexture = pGraphics->TextureLoadPNG("./icons/02d.png",true);
 
-    return (new eui::Element(s))->SetOnUpdate([](eui::ElementPtr e)
+    return (new eui::Element(s))->SetOnUpdate([this](eui::ElementPtr e)
     {
-        static float anim = 0.0f;
+        if( mAnimate )
+        {
+            static float anim = 0.0f;
 
-        anim += 0.1f;
+            anim += 0.1f;
 
-        float y = (std::sin(anim)+1.0f) * 0.1f;
-        // Play around with the padding to make it move.
-        e->SetPadding(0.0f,1.0f,y,1.0f - (0.2f - y));
+            float y = (std::sin(anim)+1.0f) * 0.1f;
+            // Play around with the padding to make it move.
+            e->SetPadding(0.0f,1.0f,y,1.0f - (0.2f - y));
+        }
 
         return true;
     });
+}
+
+eui::ElementPtr MyUI::MakeOptions(int pFont)
+{
+    eui::Style s;
+    s.mBackground = eui::COLOUR_GREY;
+    eui::ElementPtr root = new eui::Element(s);
+
+    root->SetGrid(1,4)->SetPadding(0.05f);
+    root->Attach((new eui::Checkbox("Animate Cloud",pFont))->Bind(&mAnimate)->SetPadding(0.05f)->SetPos(0,0));
+
+    root->Attach((new eui::Checkbox("Option 2",pFont))->SetPadding(0.05f)->SetPos(0,1));
+    root->Attach((new eui::Checkbox("Option 3",pFont))->SetPadding(0.05f)->SetPos(0,2));
+    root->Attach((new eui::Checkbox("Option 4",pFont))->SetPadding(0.05f)->SetPos(0,3));
+
+    return root;
+}
+
+eui::ElementPtr MyUI::MakeRadio(int pFont)
+{
+    eui::Style s;
+    s.mBackground = eui::COLOUR_GREY;
+   
+    eui::RadioButtonGroupPtr group = new eui::RadioButtonGroup;
+    group->SetStyle(s);
+    group->Add("Banana",banana);
+    group->Add("Cherry",cherry);
+    group->Add("Grapes",grapes);
+    group->Add("Orange",orange);
+    group->SetOnPressed([this](eui::RadioButtonPtr pButton,uint32_t pIndex)
+    {
+        mCurrentImage = pIndex;
+    });
+
+    group->SetPressed(banana);
+
+    return group;
 }
 
 int main(const int argc,const char *argv[])
